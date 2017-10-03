ed5a6cdc99dccfcc5f71c42155935b9a3ea2600c
Im Khem <im.khem@gmail.com>
2017-10-02 23:20:02 -0700

    Add Button & Footer

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const chalk = require('chalk');
@@ -53,7 +54,10 @@ app.use(passport.session());
 app.use((req, res, next) => {
   res.locals.user = req.user || null;
   next();
-})
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use Routes
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index/welcome');
-})
+});
 
 router.get('/dashboard', (req, res) => {
   res.render('index/dashboard');
-})
+});
+
+router.get('/about', (req, res) => {
+  res.render('index/about');
+});
 
 module.exports = router;
