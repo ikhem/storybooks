b1213c0a491bedcc8060b64b4960c4f62edebf2f
Im Khem <im.khem@gmail.com>
2017-10-04 20:37:22 -0700

    Handlebars Helpers

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ const stories = require('./routes/stories');
 // Load Keys
 const keys = require('./config/keys');
 
+// Handlebars Helpers
+const {
+  truncate,
+  stripTags
+} = require('./helpers/hbs');
+
 // Map globabl promises
 mongoose.Promise = global.Promise;
 
@@ -40,6 +46,10 @@ app.use(bodyParser.json());
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
+  helpers: {
+    truncate: truncate,
+    stripTags: stripTags
+  },
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
